20752bc7cc166e052754a2940cb03ca6b53d49dc
SathyaRaghunathan <36710665+SathyaRaghunathan@users.noreply.github.com>
2026-06-08 09:19:52 -0400

    Linkedin SQL Interview Question Solution 1 - redone

--- Data Science Skills LinkedIn SQL Interview Question Solution 1.sql
+++ Data Science Skills LinkedIn SQL Interview Question Solution 1.sql
@@ -43,5 +43,4 @@ PostgreSQL.
 SELECT candidate_id FROM candidates
 WHERE skill in ('Python','Tableau','PostgreSQL')
 GROUP BY candidate_id
-HAVING COUNT(distinct skill) =3
-ORDER BY candidate_id
+HAVING COUNT(skill)=3
