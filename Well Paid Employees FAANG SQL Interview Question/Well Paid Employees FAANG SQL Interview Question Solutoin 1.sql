7ba86267324ea06f458da832f1a9bf39a6257505
SathyaRaghunathan <36710665+SathyaRaghunathan@users.noreply.github.com>
2026-06-15 10:37:10 -0400

    well paid employees

---
+++ Well Paid Employees FAANG SQL Interview Question/Well Paid Employees FAANG SQL Interview Question Solutoin 1.sql
@@ -0,0 +1,35 @@
+'
+Companies often perform salary analyses to ensure fair compensation practices. One useful analysis is to check if there are any employees earning more than their direct managers.
+
+As a HR Analyst, you\'re asked to identify all employees who earn more than their direct managers.
+ The result should include the employee\'s ID and name.
+
+employee Schema:
+column_name	type	description
+employee_id	integer	The unique ID of the employee.
+name	string	The name of the employee.
+salary	integer	The salary of the employee.
+department_id	integer	The department ID of the employee.
+manager_id	integer	The manager ID of the employee.
+employee Example Input:
+employee_id	name	salary	department_id	manager_id
+1	Emma Thompson	3800	1	6
+2	Daniel Rodriguez	2230	1	7
+3	Olivia Smith	7000	1	8
+4	Noah Johnson	6800	2	9
+5	Sophia Martinez	1750	1	11
+6	Liam Brown	13000	3	NULL
+7	Ava Garcia	12500	3	NULL
+8	William Davis	6800	2	NULL
+Example Output:
+employee_id	employee_name
+3	Olivia Smith
+The output shows that Olivia Smith earns $7,000, surpassing her manager, William David who earns $6,800.
+'
+
+WITH CTE1 AS (SELECT e1.employee_id AS emp_id, e1.name AS emp_name,
+e1.salary AS emp_salary,
+e2.name AS man_name, e2.salary AS man_salary 
+FROM employee e1
+JOIN employee e2 on e1.manager_id = e2.employee_id)
+SELECT emp_id, emp_name FROM CTE1 WHERE emp_salary > man_salary
